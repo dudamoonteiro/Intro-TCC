81467365d2b980ee4cfcc7adfe7b659122064995
dudamoonteiro <eduardarodriguesmonteiro33@gmail.com>
2025-04-16 17:12:43 -0300

    16-04

--- src/app/adicionarEntregas/page.js
+++ src/app/adicionarEntregas/page.js
@@ -8,17 +8,27 @@ function AdiconarEntregas() {
         <>
             <header>
                 <div className="navbar">
-                <div className='logo'><img src="/icone.png" alt="" />
-                    <h1>COND TRACK</h1>
-                    <h5>Entregas Pendentes</h5>
+                    <div className='logo'><img src="/icone.png" alt="" />
+                        <h1>COND TRACK</h1>
+                        <h5>Entregas Pendentes</h5>
                     </div>
                 </div>
                 <div className="barra"></div>
             </header>
 
             <main>
-                <div className='grande'>
-                <div className='entrega'><img src="/caminhao.png" alt="" /><img src="/_ _ _ _ _ _ _ _ _ _ _.png" alt="" /><img src="/user.png" alt="" /></div>
+                <div className="grande">
+                    <div className="entrega">
+                        <div className="caminhao">
+                            <img src="/caminhao.png" alt="Caminhão" />
+                        </div>
+                        <div className="risco">
+                            <img src="/_ _ _ _ _ _ _ _ _ _ _.png" alt="Rota" />
+                        </div>
+                        <div className="pessoa">
+                            <img src="/user.png" alt="Usuário" />
+                        </div>
+                    </div>
                 </div>
 
                 <div className='barras'>
@@ -27,8 +37,7 @@ function AdiconarEntregas() {
                     <div className='pendentes'> <p>Ricardo Alves Mendes – Residente responsável pelo ramal 3</p>
                         <div className='botao'></div>
                     </div>
-                    <div className='pendentes'> <p>Juliana Pereira Santos – Ocupante principal do ramal 8</p>
-                        <div className='botao'></div></div>
+
                 </div>
             </main>
         </>
